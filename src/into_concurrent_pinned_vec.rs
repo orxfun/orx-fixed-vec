71f5dae40eedfe482c9d38bc94868afb0fdbf61c
Ugur Arikan (DHL Data & Analytics) <ugur.arikan@dpdhl.com>
2024-08-12 08:17:52 +0200

    fill with methods to enable initialized growth
    `into_concurrent_filled_with` and `grow_to_and_fill_with` methods are implemented to enable data structures which always have an initialized and valid state.

--- src/into_concurrent_pinned_vec.rs
+++ src/into_concurrent_pinned_vec.rs
@@ -7,4 +7,15 @@ impl<T> IntoConcurrentPinnedVec<T> for FixedVec<T> {
     fn into_concurrent(self) -> Self::ConPinnedVec {
         self.into()
     }
+
+    fn into_concurrent_filled_with<F>(mut self, fill_with: F) -> Self::ConPinnedVec
+    where
+        F: Fn() -> T,
+    {
+        let (len, capacity) = (self.data.len(), self.data.capacity());
+        for _ in len..capacity {
+            self.data.push(fill_with());
+        }
+        self.into()
+    }
 }
