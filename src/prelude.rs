83097d13a89d1c36adaf91a742e675586513aa0a
orxfun <orx.ugur.arikan@gmail.com>
2025-02-11 12:23:47 +0100

    Upgrade PseudoDefault dependency

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,4 +1,5 @@
 pub use crate::FixedVec;
+pub use orx_iterable::{Collection, CollectionMut, Iterable};
 pub use orx_pinned_vec::{
     ConcurrentPinnedVec, IntoConcurrentPinnedVec, PinnedVec, PinnedVecGrowthError,
 };
